68f42d9c37d2a0b14b1199c1229f7440a1e8a996
Chief-Head-Chef <plaxcale@gmail.com>
2025-04-19 16:01:17 +1000

    restored base java file, added woodrod item file

--- Dev/src/main/java/com/Fishblock/FishblockMod.java
+++ Dev/src/main/java/com/Fishblock/FishblockMod.java
@@ -18,7 +18,9 @@ public class FishBlockMod implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
+		ModItems.initialize();
 
 		LOGGER.info("Hello Fabric world!");
 	}
+
 }
